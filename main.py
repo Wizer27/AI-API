e4769f411fe7356ae9b72463f06d188bcfa25c29
Wizer27 <svd2yar@gmail.com>
2025-09-22 10:06:25 +0300

    Default

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from pydantic import BaseModel,Field
+import json
+import threading
+import socket
+from typing import Union,Literal,List,Optional
+import random
+from pydantic.types import StrictStr
+from pydantic_core.core_schema import str_schema
+import uuid
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main():
+    return "AI URIST API"
+
+
+
+

1eefe50d424500764b7bd0f52b231526e6467f1d
Wizer27 <svd2yar@gmail.com>
2025-09-22 10:09:47 +0300

    Login

--- main.py
+++ main.py
@@ -18,5 +18,18 @@ async def main():
     return "AI URIST API"
 
 
+class Login(BaseModel):
+    username:str
+    passw:str
 
+@app.post("/login")
+async def login(request:Login) -> bool:
+    with open("users.json","r") as file:
+        data = json.load(file)
 
+    if request.username in data:
+        return data[request.username] == request.passw
+    raise HTTPException(status_code=404,detail="User not found")
+    
+
+    

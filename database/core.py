e918e0a3645dbe5c8519a77c86e60241fcca6c7f
Wizer27 <svd2yar@gmail.com>
2025-12-01 21:30:56 +0300

    is_user_exists

--- database/core.py
+++ database/core.py
@@ -1,9 +1,22 @@
-from sqlalchemy import text
+from sqlalchemy import text,select
 from sql_cli import sync_engine
-from models import metadata_obj
+from models import metadata_obj,users_table
 
 
 def create_tables():
+    #metadata_obj.drop_all(sync_engine)
     metadata_obj.create_all(sync_engine)
 
-create_tables()
+def is_user_exists(username:str) -> bool:
+    with sync_engine.connect() as conn:
+        stmt = select(text("COUNT(0)")).where(users_table.c.username == username)
+        res = conn.execute(stmt)
+        print(res.fetchone())
+        return res.fetchone() is not None
+
+
+
+def register_new_user(username:str,hash_psw:str):
+    with sync_engine.connect() as conn:
+        stmt = "INSERT"
+print(is_user_exists(""))        
--- database/models.py
+++ database/models.py
@@ -6,7 +6,7 @@ metadata_obj = MetaData()
 users_table = Table("users",
                     metadata_obj,
                     Column("username",String,primary_key=True),
-                    Column("hash_psw",String,primary_key = True)
+                    Column("hash_psw",String)
                     )
 
 

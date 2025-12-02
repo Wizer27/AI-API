46a8672792824894c8a0c383920fff83b4b3996b
Wizer27 <svd2yar@gmail.com>
2025-12-02 12:23:32 +0300

    core fix

--- database/core.py
+++ database/core.py
@@ -2,10 +2,10 @@ from sqlalchemy import text,select
 from sql_cli import sync_engine
 from models import metadata_obj,users_table
 from security.hash_psw import hash_password
+import uuid
 
 
 def create_tables():
-    #metadata_obj.drop_all(sync_engine)
     metadata_obj.create_all(sync_engine)
 
 def is_user_exists(username:str) -> bool:
@@ -47,7 +47,35 @@ def login(username:str,psw:str) -> bool:
             print(f"Error : {e}")
             return False  
 
-print(register_new_user("us","1"))
-print("-----------")
-print(login("us","1"))
+def add_chat_to_user(username:str,chat_data:dict) -> bool:
+    if not is_user_exists(username):
+        return False
+    with sync_engine.connect() as conn:
+        try:
+            stmt = select(users_table.c.chats).where(users_table.c.username == username)
+            current_chats = conn.scalar(stmt)
+            if current_chats is None:
+                current_chats = []
+            update = current_chats + [chat_data]
+            update_stmt = users_table.update().where(users_table.c.username == username).values(chats = update)
+            conn.execute(update_stmt)
+            conn.commit()
+            return True
+        except Exception as e:
+            print(f"Error : {e}")
+            return False
+
+
+def debug():
+    print(register_new_user("us","1"))
+    print("-----------")
+    print(login("us","1")) 
+print(add_chat_to_user(
+    username = "us",
+    chat_data={
+        "id":str(uuid.uuid4()),
+        "messages":[]
+    }
+))
+
      
--- database/models.py
+++ database/models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Table,Column,Integer,String,MetaData
-
+from sqlalchemy import Table,Column,Integer,String,MetaData,ARRAY
+from sqlalchemy.dialects.postgresql import JSONB
 
 metadata_obj = MetaData()
 
 users_table = Table("users",
                     metadata_obj,
                     Column("username",String,primary_key=True),
-                    Column("hash_psw",String)
+                    Column("hash_psw",String),
+                    Column("chats",JSONB)
                     )
 
 
